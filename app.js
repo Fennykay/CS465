cf9befd92356d8c1596cba650d060fd108d82b6e
Kenneth Fancher <kenneth.fancher@snhu.edu>
2024-03-24 18:55:03 -0400

    completed baseline + converted other html files to hbs and set routes

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var contactRouter = require('./app_server/routes/contact');
+var aboutRouter = require('./app_server/routes/about');
+var newsRouter = require('./app_server/routes/news');
+var mealsRouter = require('./app_server/routes/meals');
+var roomsRouter = require('./app_server/routes/rooms');
+
 var app = express();
 
 // view engine setup
@@ -26,6 +32,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/travel', travelRouter);
 app.use('/users', usersRouter);
+app.use('/contact', contactRouter);
+app.use('/about', aboutRouter);
+app.use('/news', newsRouter);
+app.use('/meals', mealsRouter);
+app.use('/rooms', roomsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
